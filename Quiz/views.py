60aa5175757c879b57744afc05481d56beba6b51
ivan <ivan.vanir@gmail.com>
2021-02-14 14:57:01 +0200

    1. Added more quizzes to the Db. 2. Added next.js which is used for iterating over the questions

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
+from django.core import serializers
 
 from Quiz.models import TextQuestion
 
 
 class QuizView(View):
     def get(self, request):
+        questions = serializers.serialize('json', TextQuestion.objects.all())
         context = {
-            'questions': TextQuestion.objects.get(id=1),
+            'questions': questions,
         }
         return render(request, 'quiz.html', context)
 
---
+++ static/java_script/next.js
@@ -0,0 +1,43 @@
+function script(questionsObjs) {
+    console.log('outside the fn');
+    let questionName = document.getElementById('name');
+    let current = document.getElementById('current');
+    let questions = document.getElementsByTagName('label');
+    document.getElementById('next').addEventListener('click', next);
+    let inputs = document.getElementsByTagName('input');
+    let curr = 0;
+    let right = 0;
+
+    ///Load first Question
+    populateLabels();
+
+
+    function next() {
+        let givenAnswer = document.querySelector('input:checked');
+        if (givenAnswer.value === questionsObjs[curr].fields.answer) {
+            right += 1;
+        }
+        givenAnswer.checked = false;
+        console.log(right);
+
+        ///load the new Question;
+        curr += 1;
+        current.textContent = `(${curr+1} of 20)`;
+        populateLabels();
+
+    }
+
+    function populateLabels() {
+        let arr = [questionsObjs[curr].fields.fi_choice, questionsObjs[curr].fields.se_choice, questionsObjs[curr].fields.th_choice, questionsObjs[curr].fields.fo_choice];
+        ///Labels
+        questionName.textContent = questionsObjs[curr].fields.name;
+        for (let n=0; n<4; n++){
+        questions[n].textContent = arr[n];
+        }
+
+        ///Inputs
+        for (let n=0; n<4; n++){
+        inputs[n].value = arr[n];
+        }
+    }
+}
